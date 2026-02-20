03333764fc65c871f0a825ba308bfecc15258c66
ravi27yadav <ry4288@srmist.edu.in>
2026-02-20 10:52:24 +0530

    [Ravi Yadav]Added[UC1-WelComePage]

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+//version 1.0
+//author ravi
+//useCase 1: Welcome Page
+public class PalindromeCheckerApp {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
